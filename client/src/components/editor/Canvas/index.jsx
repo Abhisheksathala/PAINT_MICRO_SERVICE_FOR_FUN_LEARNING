2eb2c39d37e78ed5bd81b8867b1f77d62745e61d
abhishek <abhisheksathala296@gmail.com>
2026-03-06 01:45:03 +1300

    ADD

--- client/src/components/editor/Canvas/index.jsx
+++ client/src/components/editor/Canvas/index.jsx
@@ -1,22 +1,72 @@
-'use client'
-import React, { useEffect, useRef } from 'react'
+"use client";
+import { initializeFabric } from "@/fabric/Fabricutiles";
+import { useEditorStore } from "@/store/store";
+import React, { useEffect, useRef } from "react";
 
 const Canvas = () => {
+  const canvasRef = useRef(null);
+  const canvasContainerRef = useRef(null);
+  const fabriccanvasRef = useRef(null);
+  const initAttemptedRef = useRef(false);
 
-  const canvas = useRef(null)
-  const canvasContainerRef = useRef(null)
-  const fabriccanvasRef = useRef(null)
-  const initAttemptedRef = useRef(false)
+  const { setCanvas } = useEditorStore();
 
+  useEffect(() => {
+    const cleanUpCanvas = () => {
+      if (fabriccanvasRef.current) {
+        try {
+          fabriccanvasRef.current.dispose();
+        } catch (error) {
+          console.error("error disposing canvas", error);
+        }
+        fabriccanvasRef.current = null;
+        setCanvas(null);
+      }
+    };
+    cleanUpCanvas();
+    // rese init flag
+    initAttemptedRef.current = false;
 
-  useEffect(()=>{
+    // init our canvas
+
+    const initcanavs = async () => {
+      if (
+        typeof window === undefined ||
+        !canvasRef.current ||
+        initAttemptedRef.current
+      ) {
+        return;
+      }
+
+      initAttemptedRef.current = true;
+
+      try {
+        const fabricCanavs = await initializeFabric(
+          canvasRef.current,
+          canvasContainerRef.current,
+        );
+        if (!fabricCanavs) {
+          console.error("failed to load canvas js");
+          return;
+        }
+        fabriccanvasRef.current = fabricCanavs;
+        // set canavs in the store
+        setCanvas(fabricCanavs);
+        console.log("canvas init is done and set in store");
+
+        //TODO:apply custome style for the controllers
+      } catch (error) {
+        console.error("failed to load canvas js", error);
+      }
+    };
+
+    let timer = setTimeout(() => {
+      
+    }, 5000);
     
-  },[])
+  }, []);
 
+  return <div>Canvas</div>;
+};
 
-  return (
-    <div>Canvas</div>
-  )
-}
-
-export default Canvas
+export default Canvas;
