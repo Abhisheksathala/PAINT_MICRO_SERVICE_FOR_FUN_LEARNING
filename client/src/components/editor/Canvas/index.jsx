4e9e5a5fc4594cdb0cc15687938cdfb683bb4e76
abhishek <abhisheksathala296@gmail.com>
2026-03-06 00:12:10 +1300

    add

--- client/src/components/editor/Canvas/index.jsx
+++ client/src/components/editor/Canvas/index.jsx
@@ -1,11 +1,17 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const Canvas = () => {
 
   const canvas = useRef(null)
   const canvasContainerRef = useRef(null)
   const fabriccanvasRef = useRef(null)
+  const initAttemptedRef = useRef(false)
+
+
+  useEffect(()=>{
+    
+  },[])
 
 
   return (
