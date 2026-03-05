a4520526ee79515680ed4e8e5cac7b439893cad3
abhishek <abhisheksathala296@gmail.com>
2026-03-06 00:10:23 +1300

    add

--- client/src/components/editor/Canvas/index.jsx
+++ client/src/components/editor/Canvas/index.jsx
@@ -4,6 +4,9 @@ import React, { useRef } from 'react'
 const Canvas = () => {
 
   const canvas = useRef(null)
+  const canvasContainerRef = useRef(null)
+  const fabriccanvasRef = useRef(null)
+
 
   return (
     <div>Canvas</div>
