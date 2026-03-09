74dcfbb590b1e20590e2fdd9bfbe794e5c78779f
abhishek <abhisheksathala296@gmail.com>
2026-03-09 22:59:24 +0530

    add

--- client/src/components/editor/Sidebar/index.jsx
+++ client/src/components/editor/Sidebar/index.jsx
@@ -4,10 +4,34 @@ import React, { useState } from "react";
 import ElementsPanels from "./panels/Elements";
 import { Type } from "lucide-react";
 import Textpannel from "./panels/Text";
+import Drawpannel from "./panels/Draw";
+import Settingspannel from "./panels/Settings";
+import Aipannel from "./panels/Ai";
+import { Pencil } from "lucide-react";
+import { Settings } from "lucide-react";
+import { Sparkle } from "lucide-react";
+import { Upload } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
+import { ChevronsRightLeftIcon } from "lucide-react";
 
 const Sidebar = () => {
   const [isPanelCollapsed, setIspannelcollapsed] = useState(false);
   const [activeSidebar, setActiveSidebar] = useState(null);
+
+  const handleItemClick = (id) => {
+    if (id === activeSidebar && !isPanelCollapsed) return;
+    setActiveSidebar(id);
+    setIspannelcollapsed(false);
+  };
+
+  const closeSecondaryPanel = (e) => {
+    setActiveSidebar(null);
+  };
+
+  const togglepanelCollapse = (e) => {
+    // e.stopPrpagation();
+    setIspannelcollapsed(!isPanelCollapsed);
+  };
 
   const sidebarItems = [
     {
@@ -22,22 +46,76 @@ const Sidebar = () => {
       labal: "Text",
       panel: () => Textpannel,
     },
-
+    {
+      id: "Uploads",
+      icons: Upload,
+      labal: "Upload",
+      panel: () => Textpannel,
+    },
+    {
+      id: "Draw",
+      icons: Pencil,
+      labal: "Draw",
+      panel: () => Drawpannel,
+    },
+    {
+      id: "settings",
+      icons: Settings,
+      labal: "settings",
+      panel: () => Settingspannel,
+    },
+    {
+      id: "Ai",
+      icons: Sparkle,
+      labal: "Ai",
+      panel: () => Aipannel,
+    },
   ];
+
+  const activeItem = sidebarItems.find((items) => items.id === activeSidebar);
 
   return (
     <div className="flex h-full">
-      <div className="sidebar">
-        {
-          sidebarItems.map((items,index)=>{
-            return(
-              <>
-              <div className="" key={index}></div>
-              </>
-            )
-          })
-        }
-      </div>
+      <aside className="sidebar">
+        {sidebarItems.map((items, index) => {
+          const { id, labal, panel } = items;
+          return (
+            <div
+              onClick={() => {
+                handleItemClick(id);
+              }}
+              className={`sidebar-item ${activeSidebar === id ? "active" : ""}`}
+              key={index || id}
+            >
+              <items.icons className="h-5 w-5 sidebar-item-icon" />
+              <span className="sidebaritem-lable">{labal}</span>
+            </div>
+          );
+        })}
+      </aside>
+
+      {activeSidebar && (
+        <div
+          className={`secondary-panel ${isPanelCollapsed ? "collapse" : ""}`}
+
+          style={{
+            width:`${isPanelCollapsed ? '0' : '350px'}`,
+            opacity:`${isPanelCollapsed ? 0 : 1}`,
+            overflow:`${isPanelCollapsed ? 'hidden' : 'visible'}`
+          }}
+        >
+          <div className="panel-header">
+            <button className="back-button" onClick={closeSecondaryPanel}>
+              <ArrowLeft />
+            </button>
+            <span className="panel-title">{activeItem.labal}</span>
+          </div>
+          <div className="panel-content">{activeItem.panel()}</div>
+          <button className="collapse-button" onClick={togglepanelCollapse}>
+            <ChevronsRightLeftIcon className="w-5 h-5" />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
