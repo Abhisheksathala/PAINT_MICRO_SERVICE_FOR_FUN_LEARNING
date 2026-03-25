17c13478b407bd0456a784e4da9b15d54d3d75ad
abhishek <abhisheksathala296@gmail.com>
2026-03-25 22:43:04 +0530

    add

--- client/src/components/editor/properties/index.jsx
+++ client/src/components/editor/properties/index.jsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useEditorStore } from "@/store/store";
 import React from "react";
 
 const Properties = () => {
+    const {  canvas } = useEditorStore();
   return (
     <div className="fixed flex right-0 top-[56px] bottom-[0px] w-[280px] bg-white border-1 border-gray-200 z-10">
       <div className="flex items-center justify-between p-3 border-b">
